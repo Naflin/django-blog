502839ac062aabf1ff4b7a480d52646f48abcc51
Naflin <nathanblue00@gmail.com>
2019-01-08 21:43:12 -0500

    added RSS feed and blog post search

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,37 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post, Comment
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.objects.annotate(
+                search=SearchVector('title','body'),
+            ).filter(search=query)
+            # search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            # results = Post.objects.annotate(
+                # similarity=TrigramSimilarity('title',query)
+            # ).filter(similarity__gt=0.3).order_by('-similarity')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
 
 def post_share(request, post_id):
     # Retrie post by id
